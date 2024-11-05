1145c5b00c0f3776bbda20de500389fecff314ad
Tiananmen Setyo Nugroho <tnanmen96@gmail.com>
2024-11-05 09:35:52 +0700

    Update dice simulation.py

--- dice simulation.py
+++ dice simulation.py
@@ -1,18 +1,18 @@
 import random
 
 def dice_simulation():
-    print("Selamat datang di simulasi lempar dadu!")
+    print("Welcome to the dice throwing simulation!")
     
     while True:
-        roll = input("Ketik 'y' untuk lempar dadu atau 'n' untuk keluar: ").lower()
+        roll = input("Type 'y' to roll the dice or 'n' to exit: ").lower()
         
         if roll == 'y':
             dice_result = random.randint(1, 6)
-            print(f"Anda mendapat angka: {dice_result}")
+            print(f"You got a number: {dice_result}")
         elif roll == 'n':
-            print("Terima kasih telah menggunakan simulasi dadu. Sampai jumpa!")
+            print("Thank you for using the dice simulation. See you later.!")
             break
         else:
-            print("Input tidak valid. Silakan ketik 'y' untuk lempar dadu atau 'n' untuk keluar.")
+            print("Invalid input. Please type 'y' to roll the dice or 'n' to exit.")
 
 dice_simulation()
